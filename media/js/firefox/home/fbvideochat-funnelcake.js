734f7db19aac6efe8de902ae2c726015badf379f
Jon Petto <jonpetto@gmail.com>
2018-05-12 11:43:36 -0400

    [fix bug 1458340] Add FB videochat funnelcake banner to /firefox/.

---
+++ media/js/firefox/home/fbvideochat-funnelcake.js
@@ -0,0 +1,45 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+(function ($, Mozilla) {
+    'use strict';
+
+    var $fbVideochatBanner;
+
+    var ieRegex = /MSIE|Trident|Edge/i;
+    var isIE = ieRegex.test(window.navigator.userAgent);
+
+    var fbRegex = /facebook\.com/i;
+    var isFromFb = fbRegex.test(document.referrer);
+
+    var hasCookies = typeof Mozilla.Cookies !== 'undefined' && Mozilla.Cookies.enabled();
+
+    if (hasCookies && window.site.platform === 'windows' && isFromFb && isIE) {
+        // make sure visitor hasn't previously dismissed the banner
+        var wasDismissed = Mozilla.Cookies.getItem('fbvideochat-banner-dismiss');
+
+        if (wasDismissed === null) {
+            $fbVideochatBanner = $('#fbvideochat-banner');
+
+            // append funnelcake ID to Windows download links
+            $('#download-fbvideochat').find('.os_win a, .os_win64 a').each(function(i, aTag) {
+                aTag.href += '?f=134';
+            });
+
+            $('#fbvideochat-close').one('click', function() {
+                // don't show the banner again for 7 days
+                var d = new Date();
+                d.setTime(d.getTime() + (7 * 24 * 60 * 60 * 1000));
+                Mozilla.Cookies.setItem('fbvideochat-banner-dismiss', true, d.toUTCString(), '/');
+
+                $fbVideochatBanner.slideUp('fast');
+            });
+
+            // slight delay before sliding down the banner
+            setTimeout(function() {
+                $fbVideochatBanner.slideDown();
+            }, 500);
+        }
+    }
+})(window.jQuery, window.Mozilla);
