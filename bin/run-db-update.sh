b3e4f51de4d89a2a643f75aa5866424b0145b4c7
Paul McLanahan <pmac@mozilla.com>
2018-05-22 23:02:48 -0400

    Force git repo updates during deploy

--- bin/run-db-update.sh
+++ bin/run-db-update.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 set -ex
 
+if [[ "$2" == "--force" ]]; then
+    python manage.py update_security_advisories --quiet --clear-db
+    python manage.py update_release_notes --quiet --force
+    python manage.py update_externalfiles --quiet --force
+    python manage.py update_www_config --quiet --force
+else
+    python manage.py update_security_advisories --quiet
+    python manage.py update_release_notes --quiet
+    python manage.py update_externalfiles --quiet
+    python manage.py update_www_config --quiet
+fi
 python manage.py update_product_details_files
-python manage.py update_security_advisories --quiet
 python manage.py update_wordpress --quiet
-python manage.py update_release_notes --quiet
-python manage.py update_externalfiles --quiet
 python manage.py update_newsletter_data --quiet
-python manage.py update_www_config --quiet
 
 if [[ "$1" == "--all" ]]; then
     # less frequent. these will modify the DB every time.
