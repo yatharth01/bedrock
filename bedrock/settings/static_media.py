8c85085e741bcfe94c9922f8f55e374d12cdc1b8
Craig Cook <craigcook@users.noreply.github.com>
2018-05-29 11:00:37 -0700

    [bug 1463821] What’s New page for Firefox 61

--- bedrock/settings/static_media.py
+++ bedrock/settings/static_media.py
@@ -465,6 +465,12 @@ PIPELINE_CSS = {
         ),
         'output_filename': 'css/firefox_whatsnew_60-bundle.css',
     },
+    'firefox_whatsnew_61': {
+        'source_filenames': (
+            'css/firefox/whatsnew/whatsnew-61.scss',
+        ),
+        'output_filename': 'css/firefox_whatsnew_61-bundle.css',
+    },
     'firefox_whatsnew': {
         'source_filenames': (
             'css/firefox/whatsnew/whatsnew.scss',
@@ -1156,6 +1162,14 @@ PIPELINE_JS = {
             'js/firefox/whatsnew/whatsnew-60.js',
         ),
         'output_filename': 'js/firefox_whatsnew_60-bundle.js',
+    },
+    'firefox_whatsnew_61': {
+        'source_filenames': (
+            'js/base/mozilla-fxa-iframe.js',
+            'js/base/send-to-device.js',
+            'js/firefox/whatsnew/whatsnew-61.js',
+        ),
+        'output_filename': 'js/firefox_whatsnew_61-bundle.js',
     },
     'firefox_whatsnew_fxa': {
         'source_filenames': (
